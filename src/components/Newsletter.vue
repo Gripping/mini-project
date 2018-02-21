cb67a92c4402113fc6fead7bb9cad1f8bea862eb
Niclas Gripping <niclas@gripping.nu>
2018-02-21 15:41:51 +0100

    Added newsletter component and SoMe component

---
+++ src/components/Newsletter.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="newsletter-container">
+    <form class="newsletter-form">
+        <input class="newsletter-form__input" type="email" placeholder="Your email" />
+        <input class="newsletter-form__submit" type="submit" value="subscribe to our newsletter">
+    </form>
+  </section>
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
---
+++ src/components/SoMe.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="social-media">
+    <h1 class="social-media__title">Get us on</h1>
+    <ul class="social-media__list">
+      <li class="media"><router-link to=""><img src="/static/img/SoMe/twitter.svg" alt=""></router-link></li>
+      <li class="media"><router-link to=""><img src="/static/img/SoMe/pinterest.svg" alt=""></router-link></li>
+      <li class="media"><router-link to=""><img src="/static/img/SoMe/facebook.svg" alt=""></router-link></li>
+      <li class="media"><router-link to=""><img src="/static/img/SoMe/google.svg" alt=""></router-link></li>
+      <li class="media"><router-link to=""><img src="/static/img/SoMe/youtube.svg" alt=""></router-link></li>
+      <li class="media"><router-link to=""><img src="/static/img/SoMe/tumblr.svg" alt=""></router-link></li>
+    </ul>
+  </section>
+</template>
+<script>
+
+</script>
+<style lang="scss">
+
+</style>
