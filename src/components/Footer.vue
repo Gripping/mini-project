58595703dc2d7c0af5447acb4f3f741336883097
Niclas Gripping <niclas@gripping.nu>
2018-02-21 15:36:51 +0100

    Added footer & header

---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer class="main-footer container">
+    <div class="main-footer__questions">
+      <p class="title">Questions?</p>
+      <router-link to="">Help</router-link>
+      <router-link to="">Track Order</router-link>
+      <router-link to="">Returns</router-link>
+    </div>
+    <div class="main-footer__location">
+      <p class="title">Where to buy</p>
+      <router-link to="">Store locator</router-link>
+      <router-link to="">Wishlist</router-link>
+      <router-link to="">Giftcard</router-link>
+    </div>
+    <div class="main-footer__about">
+      <p class="title">About us</p>
+      <router-link to="">Our Story</router-link>
+      <router-link to="">Careers</router-link>
+      <router-link to="">Affiliates</router-link>
+    </div>
+    <div class="main-footer__payment">
+      <img src="/static/img/payment.png" alt="">
+    </div>
+    <div class="main-footer__logo">
+      <img src="/static/img/logo.svg" alt="">
+    </div>
+  </footer>
+</template>
+<script>
+
+</script>
+<style lang="scss">
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,76 @@
+<template>
+    <header class="nav-header">
+      <router-link class="nav-header__logo" to="/"><img src="/static/img/logo.svg" alt="Classy logo"></router-link>
+
+      <ul class="user-links">
+        <li class="user-links__account"><router-link to=""><img src="/static/img/icons/login.svg" alt="">Login</router-link></li>
+        <li class="user-links__cart"><router-link to=""><img src="/static/img/icons/cart.svg" alt="">Cart</router-link></li>
+        <li class="user-links__search"><router-link to=""><img src="/static/img/icons/search.svg" alt=""></router-link></li>
+        <li class="user-links__menu">
+            <div @click.prevent='activateNav()' class="hamburger">
+                <div class="hamburger__patty"></div>
+                <div class="hamburger__patty"></div>
+                <div class="hamburger__patty"></div>
+            </div>
+        </li>
+      </ul>
+      <nav class="main-nav">
+        <router-link class="main-nav__logo" v-on:click.native="activateNav" to="/"><img src="/static/img/logo.svg" alt="Classy logo"></router-link>
+
+        <ul class="main-nav__links">
+          <li class="nav-link"><router-link v-on:click.native="activateNav" class="nav-link__collection" to="/CategoryPage">Collection</router-link></li>
+          <li class="nav-link"><router-link v-on:click.native="activateNav" class="nav-link__contact" to="/ContactPage">Contact</router-link></li>
+          <li class="nav-link"><router-link v-on:click.native="activateNav" class="nav-link__about" to="">About</router-link></li>
+          <li class="nav-link"><router-link v-on:click.native="activateNav" class="nav-link__blog" to="">Blog</router-link></li>
+        </ul>
+      </nav>
+    </header>
+</template>
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+
+    }
+  },
+  mounted: function() {
+    this.changeMenu();
+  },
+  watch: {
+    '$route': function () {
+      this.changeMenu();
+    }
+  },
+  methods: {
+    changeMenu: function() {
+      var x = document.querySelector('.nav-header');
+      if(this.$route.name == 'SinglePage'){
+        x.className += ' single-page';
+      }else{
+        if(x.classList.contains('single-page')){
+          x.classList.remove('single-page');
+        }
+      }
+    },
+    activateNav: function() {
+      let nav = document.querySelector('.main-nav');
+      let hamburger = document.querySelector('.hamburger');
+      let body = document.body;
+      if(!document.querySelector('.hamburger').classList.contains('nav--active')){
+        nav.classList += ' nav--active';
+        hamburger.classList += ' nav--active';
+        body.classList += ' nav--active';
+      }else{
+        console.log("test");
+        nav.classList.remove('nav--active');
+        hamburger.classList.remove('nav--active');
+        body.classList.remove('nav--active');
+      }
+    }
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
